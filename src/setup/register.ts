9d31f2a799586d4fb3ec114c0377255d7870cb12
NHeroOP <79276833+NHeroOP@users.noreply.github.com>
2026-07-19 02:04:17 +0530

    refactor: remove async from registerHandlers and clean up function structure

--- src/setup/register.ts
+++ src/setup/register.ts
@@ -1,5 +1,11 @@
 import type { Server as SocketServer, Socket } from "socket.io";
+import { roomListener } from "@/modules/room/listener.js";
+import { gameListener } from "@/modules/game/listener.js";
+import { questionListener } from "@/modules/question/listener.js";
 
-export async function registerHandlers(io: SocketServer, socket: Socket) {
-  //
+export function registerHandlers(io: SocketServer, socket: Socket) {
+  roomListener(io, socket);
+  gameListener(io, socket);
+  questionListener(io, socket);
 }
+ 
